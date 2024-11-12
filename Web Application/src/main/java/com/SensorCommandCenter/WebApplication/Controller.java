e5f3eb3a966d57ab95914b2f612f3da3e6e973f5
Charis <charisgrubb@gmail.com>
2024-11-12 13:05:21 -0500

    Adding ThymeLeaf for the dynamic web engine

--- Web Application/src/main/java/com/SensorCommandCenter/WebApplication/Controller.java
+++ Web Application/src/main/java/com/SensorCommandCenter/WebApplication/Controller.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class Controller{
 
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "index";
     }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Return All users";
+    }
+
 
 }
