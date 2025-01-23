3f3c7cc8d8a57dac9711eba23f8fb9ef43d694c1
Charis Grubb <charisgrubb@gmail.com>
2025-01-23 11:03:46 -0500

    Testing web param requests

--- Web Application/src/main/java/com/SensorCommandCenter/WebApplication/UserController.java
+++ Web Application/src/main/java/com/SensorCommandCenter/WebApplication/UserController.java
@@ -4,6 +4,7 @@ package com.SensorCommandCenter.WebApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,12 +14,12 @@ public class UserController {
 
     @GetMapping("/users")
     public String getUsers(Model model) {
-        model.addAttribute("something", "This is a val");
+        model.addAttribute("user_list", "Users:");
         return "users";
     }
 
     @GetMapping("/user")
-    public String getUser(Model model) {
+    public String getUser(@RequestParam(name="username", required = false) String username, Model model) {
         model.addAttribute("something", "This is a val");
         return "User Details";
     }
